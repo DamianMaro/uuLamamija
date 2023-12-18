89cf2af8511c403d7448a6e02a989ca37822710a
Legy <jakubkolegat@gmail.com>
2023-12-18 15:36:50 +0100

    cart/update

--- uu_lamamija_maing01-server/app/api/validation_types/cart-types.js
+++ uu_lamamija_maing01-server/app/api/validation_types/cart-types.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 const cartUpdateDtoInType = shape({
-  id: { type: "string", required: true },
-  nazov: { type: "string", minLength: 1, maxLength: 50, required: true },
-  cena: { type: "float", required: true },
-  pocet: { type: "int", required: true },
+  id: id().isRequired(),
+  name: string(50).isRequired(),
+  price: float(10).isRequired(),
+  count: integer(10).isRequired()
 });
